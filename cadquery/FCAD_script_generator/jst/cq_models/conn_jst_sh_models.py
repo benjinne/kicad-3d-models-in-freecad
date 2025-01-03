4c9248ec4f6b0a20cd05a98e2b651ca09aa8d330
benjinne <benjinne@gmail.com>
2025-01-03 18:58:07 -0500

    sh angled sm finished

--- cadquery/FCAD_script_generator/jst/cq_models/conn_jst_sh_models.py
+++ cadquery/FCAD_script_generator/jst/cq_models/conn_jst_sh_models.py
@@ -110,30 +110,35 @@ def generate_straight_pins(params):
 def generate_angled_pins(params):
     num_pins = params.num_pins
     pin_distance = (num_pins-1)*pin_pitch
+
     mount_pin = cq.Workplane("YZ").workplane(-pin_width/2)\
-        .move(-2.475,0).vLine(2.05).hLine(2.2).vLine(-0.54).hLine(-1.55)\
-        .vLine(-0.71).hLine(2.06).vLine(-0.43).hLine(-0.49)\
-        .vLine(-0.37).close().extrude(pin_width)
+    	.move(-2.475,0.01).vLine(1.25).hLine(0.5).vLine(-0.65)\
+        .hLine(1.0).vLine(-0.6)\
+        .close().extrude(pin_width)
 
     signal_pin = cq.Workplane("YZ").workplane(-pin_distance/2 -pin_width/2)\
-        .move(2.4754,0).vLine(0.3).hLine(-0.7).vLine(0.35).hLine(-0.67).vLine(1.4196)\
-        .hLine(-3.35).line(0.4,-0.4).vLine(0.07).hLine(2.55).vLine(-0.74).hLine(-2.4)\
-        .line(-0.4,-0.16).vLine(-0.17).hLine(2.8).vLine(-0.67).close().extrude(pin_width)
+    	.move(-1.225,1.79).vLine(0.35).hLine(1.75).vLine(-0.48)\
+        .close().extrude(pin_width)
 
-    pins = signal_pin
+    signal_pcb_pin = cq.Workplane("YZ").workplane(-pin_distance/2 -pin_width/2)\
+    	.move(1.675,0).vLine(0.06).hLine(0.1).vLine(0.14).hLine(0.7).vLine(-0.2)\
+        .close().extrude(pin_width)
+    
+    pins = signal_pin.union(signal_pcb_pin)
 
     for i in range(num_pins):
         pins = pins.union(signal_pin.translate((i*pin_pitch,0,0)))
+        pins = pins.union(signal_pcb_pin.translate((i*pin_pitch,0,0)))
 
-    pins = pins.union(mount_pin.translate((-(num_pins-1)*pin_pitch/2-1.85,0,0)))
-    pins = pins.union(mount_pin.translate(((num_pins-1)*pin_pitch/2+1.85,0,0)))
+    pins = pins.union(mount_pin.translate((-pin_distance/2-1.1,0,0)))
+    pins = pins.union(mount_pin.translate((pin_distance/2+1.1,0,0)))
     return pins
 
 def generate_angled_body(params):
     body_off_center_y = 0.0
     body = generate_straight_body(params)
     body = body.rotate((0,0,0),(1,0,0),90)
-    body = body.translate((0,body_off_center_y+1.58,1.62))
+    body = body.translate((0,body_off_center_y+1.775,1.09))
     return body
 
 def generate_body(params):
